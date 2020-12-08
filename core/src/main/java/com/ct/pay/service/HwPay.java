ebf90cfae018e58ee023aff7bd6e9da827e6919b
xiaoct <18646093318@163.com>
2020-12-08 11:25:32 +0800

    hwPay

---
+++ core/src/main/java/com/ct/pay/service/HwPay.java
@@ -0,0 +1,20 @@
+package com.ct.pay.service;
+
+/**
+ * @description:
+ * @author: Cter
+ * @date: 2020/12/8
+ */
+public class HwPay implements IPay {
+    public void query() {
+
+    }
+
+    public void pay() {
+
+    }
+
+    public void refund() {
+
+    }
+}
